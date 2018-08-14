9f5aaa4fc8e574b69368a71fe937ccb2cc61c874
Martin-Alexander <martingianna@gmail.com>
2018-08-14 16:52:43 -0400

    webpack config

---
+++ drag-n-drop/dist/bundle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("const dragMes = document.querySelectorAll(\".dragMe\");\nconst board = document.getElementById(\"board\");\nconst uploaded = document.getElementById(\"uploaded\");\n\nconst cursorOffset = {};\n\nconst dragMeDragStartHandler = event => {\n  const image = event.currentTarget;\n  const imagePosition = image.getBoundingClientRect();\n\n  cursorOffset.x = imagePosition.x - event.clientX;\n  cursorOffset.y = imagePosition.y - event.clientY;\n};\n\nconst dragMeDragEndHandler = event => {\n  const image = event.currentTarget;\n\n  image.style.left = event.pageX + cursorOffset.x;\n  image.style.top = event.pageY + cursorOffset.y;\n};\n\nconst dropHandler = event => {\n  event.preventDefault();\n\n  const fileReader = new FileReader();\n  fileReader.onload = event => {\n    uploaded.src = event.srcElement.result;\n  };\n\n  if (event.dataTransfer.files[0]) {\n    fileReader.readAsDataURL(event.dataTransfer.files[0]);\n  }\n};\n\nconst dragOverHandler = event => {\n  event.preventDefault();\n};\n\ndragMes.forEach(dragMe => {\n  dragMe.addEventListener(\"dragstart\", dragMeDragStartHandler);\n  dragMe.addEventListener(\"dragend\", dragMeDragEndHandler);\n});\n\ndocument.addEventListener(\"drop\", dropHandler);\ndocument.addEventListener(\"dragover\", dragOverHandler);\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ });
--- drag-n-drop/webpack.config.js
+++ drag-n-drop/webpack.config.js
@@ -1,1 +1,22 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  mode: "development"
+};
