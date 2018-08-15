c8888cf5177180da8eb6bf5b118e86651147fefc
Martin-Alexander <martingianna@gmail.com>
2018-08-15 17:44:35 -0400

    drag upload and drag placement with new classes

--- drag-n-drop/src/drag_and_drop/drag_and_drop.js
+++ drag-n-drop/src/drag_and_drop/drag_and_drop.js
@@ -5,6 +5,43 @@ export default class DragAndDrop {
     this.boardElement = board;
     this.trayElement = tray;
 
-    this.tray = new Tray(this.trayElement)
+    this.tray = new Tray({ trayElement: this.trayElement, dragAndDrop: this })
+    
+    this.initialize();
+  }
+  
+  initialize() {
+    document.addEventListener("drop", this._dropHandler.bind(this));
+    document.addEventListener("dragover", this._dragOverHandler.bind(this));
+  }
+
+  addImageToTray(imageElement) {
+    this.tray.addImage(imageElement);
+  }
+
+  isWithinBounds({ x, y }) {
+    return x > 0 && x < this.boardElement.clientWidth && y > 0 && y < this.boardElement.clientHeight
+  }
+
+// Private
+
+  _dropHandler(event) {
+    event.preventDefault();
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = (event) => {
+      const image = new Image();
+      image.src = event.srcElement.result;
+      this.addImageToTray(image);
+    }
+
+    if (event.dataTransfer.files[0]) {
+      fileReader.readAsDataURL(event.dataTransfer.files[0])
+    }
+  }
+
+  _dragOverHandler(event) {
+    event.preventDefault();
   }
 }
--- drag-n-drop/src/index.js
+++ drag-n-drop/src/index.js
@@ -1,24 +1,3 @@
-const dropHandler = (event) => {
-  event.preventDefault();
-
-  const fileReader = new FileReader();
-  fileReader.onload = (event) => {
-    uploaded.src = event.srcElement.result
-  }
-
-  if (event.dataTransfer.files[0]) {
-    fileReader.readAsDataURL(event.dataTransfer.files[0])
-  }
-
-  console.log(event);
-}
-
-document.addEventListener("drop", dropHandler);
-
-document.addEventListener("dragover", (event) => {
-  event.preventDefault();
-});
-
 import DragAndDrop from "./drag_and_drop/drag_and_drop.js";
 
 const board = document.querySelector("#board");
