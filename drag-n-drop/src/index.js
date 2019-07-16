c3457217d32f43d596f89284146f99b64bf9827d
Martin-Alexander <martingianna@gmail.com>
2019-07-16 14:55:08 -0400

    deleted everything

--- drag-n-drop/src/index.js
+++
@@ -1,9 +0,0 @@
-import DragAndDrop from "./drag_and_drop/drag_and_drop.js";
-
-const board = document.querySelector("#board");
-const tray = document.querySelector("#tray");
-
-global.dragAndDrop = new DragAndDrop({
-  board: board,
-  tray: tray
-});
